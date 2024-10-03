60724dba97382af4eab453420f8f87471a6558d9
shubhampaithankar <shubhampaithankar65@gmail.com>
2024-10-03 13:22:19 +0530

    fix: sorting logic, minor changes

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import asyncio
+import re
 
 from selenium_driverless import webdriver
+from selenium_driverless.types.webelement import WebElement
 from selenium_driverless.types.by import By
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 
+from modules.helper import *
+
+from config.search import *
 from config.secrets import *
 
 async def login(driver: webdriver.Chrome):
@@ -36,54 +41,51 @@ async def login(driver: webdriver.Chrome):
         await submit_button.click()
         await driver.sleep(5)
 
+        if await driver.current_url != 'https://wellfound.com/jobs':
+            await login(driver)
+
     except WebDriverException as e:
         print(f"Error during setting filters: {e}")
         raise e
 
 async def set_filters(driver: webdriver.Chrome):
     try:
-        try: sort = await driver.find_element(By.XPATH, '//button/span[text()="Recommended"]', timeout=15)
+        try: sort_by = await driver.find_element(By.XPATH, '//button/span[text()="Recommended"]', timeout=15)
         except NoSuchElementException:
             print("Sort button not found")
             raise e
 
-        await sort.click()
-        await driver.sleep(1)
+        await sort_by.click()
+        await driver.sleep(2)
 
-        try: options = await driver.find_element(By.XPATH, '//div[@id="tippy-11"]', timeout=15)
-        except NoSuchElementException:
-            print("Options not found")
-            raise e
-
-        await options.click()
-        await driver.sleep(1)
-
-        try: most_recent = await driver.find_element(By.XPATH, '//div[contains(text(), "Most Recent")]', timeout=15)
+        try: most_recent = await driver.find_element(By.XPATH, '//span[text()="See most recent jobs first"]')
         except NoSuchElementException:
             print("Most Recent option not found")
             raise e
 
-        await driver.sleep(1)
         await most_recent.click()
+        await driver.sleep(2)
     except WebDriverException as e:
         print(f"Error during setting filters: {e}")
         raise e
 
+
 async def main():
     options = webdriver.ChromeOptions()
-    options.add_argument("--incognito")
-    options.add_argument("--disable-extensions")
+    # options.add_argument("--incognito")
+
+    if not headless:
+        dark_reader = 'extension/dark-reader.zip'
+        options.add_extension(dark_reader)
 
     async with webdriver.Chrome(options=options) as driver:
         try:
             await driver.maximize_window()
 
             await driver.get('https://wellfound.com/login', wait_load=True)
+            await driver.sleep(2)
 
             await login(driver)
-
-            if await driver.current_url != 'https://wellfound.com/jobs':
-                await login(driver)
 
             await driver.sleep(1)
 
